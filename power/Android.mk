e8f346393680f21b2a6fcad4de71d9d1a5cdcd9d
Rams <ramshell688@gmail.com>
2023-06-04 22:54:13 +0700

    Revert "camellia: power: Rework implementation for newer perf stack"
    This reverts commit fef3bf4d063bf70767b6c4acb582ba74d2444270.

--- power/Android.mk
+++ power/Android.mk
@@ -5,21 +5,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.power-service-mediatek
 LOCAL_VENDOR_MODULE := true
 LOCAL_MULTILIB := 64
-LOCAL_VINTF_FRAGMENTS := power-mtk.xml
+LOCAL_VINTF_FRAGMENTS := power-mediatek.xml
 LOCAL_SRC_FILES := Power.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
     libbinder_ndk \
+    libpowerhal \
     android.hardware.power-V2-ndk_platform
 
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
-endif
-
-ifneq ($(TARGET_POWERHAL_MODE_EXT),)
-    LOCAL_CFLAGS += -DMODE_EXT
-    LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_MODE_EXT)
-endif
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 include $(BUILD_SHARED_LIBRARY)
