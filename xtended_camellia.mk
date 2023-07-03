a900993f5856c96aa69e3f4f2e640bcfc68c6cb6
ramshell688 <ramshell688@gmail.com>
2023-07-03 03:46:55 +0000

    camellia: Xtended bringup!

--- xtended_camellia.mk
+++ xtended_camellia.mk
@@ -9,24 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Inherit Pixel Launcher Extended
-$(call inherit-product, vendor/PixelLauncherExtended/PixelLauncher.mk)
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from camellia device
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
 # PixelExperience Stuff
-PIXEL_LAUNCHER_VARIANT := glance12
-ICONS_VARIANT := true
+XTENDED_BUILD_MAINTAINER := ramshell688
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_camellia
+PRODUCT_NAME := xtended_camellia
 PRODUCT_DEVICE := camellia
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_BRAND := Redmi
