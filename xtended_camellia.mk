eae63aa8b638ab03529ceffa8d39c3ded10302d7
ramshell688 <ramshell688@gmail.com>
2023-07-04 02:31:47 +0000

    camellia: Xtended bringup!

--- xtended_camellia.mk
+++ xtended_camellia.mk
@@ -1,4 +1,4 @@
-#
+0#
 # Copyright (C) 2023 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -15,7 +15,10 @@ $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
 # PixelExperience Stuff
+PROCESSOR_MODEL := Dimensity 700
 XTENDED_BUILD_MAINTAINER := ramshell688
+WITH_GAPPS := true
+TARGET_INCLUDE_NGA := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
