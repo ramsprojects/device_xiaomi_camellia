e9563c775a911bbcab4bd6021bd50015dc4cd654
ramshell688 <ramshell688@gmail.com>
2023-07-27 16:08:12 +0000

    camellia: Welcome to ProtonPlus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_camellia.mk
+    $(LOCAL_DIR)/camellia.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_camellia-user \
-    aosp_camellia-userdebug \
-    aosp_camellia-eng
+    camellia-user \
+    camellia-userdebug \
+    camellia-eng
--- camellia.mk
+++ camellia.mk
@@ -9,18 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/proton/config/common.mk)
 
 # Inherit from camellia device
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
 # PixelExperience Stuff
+WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_camellia
+PRODUCT_NAME := camellia
 PRODUCT_DEVICE := camellia
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_BRAND := Redmi
