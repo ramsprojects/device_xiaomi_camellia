03c7ce5132d04a72af17e511eca136162efff361
ramshell688 <ramshell688@gmail.com>
2023-08-24 10:50:38 +0700

    camellia: Welcome to Superior Extended

--- superior_camellia.mk
+++ superior_camellia.mk
@@ -18,6 +18,8 @@ $(call inherit-product, device/xiaomi/camellia/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+PRODUCT_SYSTEM_PROPERTIES += \
+     ro.spos.maintainer=Rams•(ramshell688)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := superior_camellia
